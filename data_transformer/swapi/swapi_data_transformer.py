810c2d6dc32c4cab94cde3d60fdb0c508ab4154a
Samuel Braganza Z <sbraganz@fiec.espol.edu.ec>
2023-03-02 13:04:47 +0100

    WIP

--- data_transformer/swapi/swapi_data_transformer.py
+++ data_transformer/swapi/swapi_data_transformer.py
@@ -8,7 +8,7 @@ class SwapiDataTransformer:
     def __init__(self, data):
         self.data = data
 
-    def transform_data(self):
+    def transform_data(self) -> etl.Table:
         table = etl.fromdicts(self.data)
         table = table.convert(
             "edited",
@@ -16,13 +16,13 @@ class SwapiDataTransformer:
                 "%Y-%m-%d"
             ),
         )
-        table = table.rename({"homeworld": "homeworld_url"})
         table = table.joinlookup(
             etl.fromjson("https://swapi.dev/api/planets/", "results"),
-            "homeworld_url",
+            "homeworld",
             "url",
             "name",
         )
+        table = table.rename({"homeworld": "homeworld_url"})
         table = table.cutout(
             "films", "species", "vehicles", "starships", "created", "edited", "url"
         )

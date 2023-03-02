ffd3bc168fb12959ec601dd53643a1c9d858ded1
Samuel Braganza Z <sbraganz@fiec.espol.edu.ec>
2023-03-02 11:34:44 +0100

    WIP

--- data_outputter/swapi/swapi_data_outputter.py
+++ data_outputter/swapi/swapi_data_outputter.py
@@ -1,25 +1,32 @@
-
 # swapi_data_outputter.py
-import petl as etl
-from swapi.models import SwapiModel
-from datetime import datetime
 import os
+from datetime import datetime
+
+import petl as etl
+
+from swapi.models import Person
+
 
 class SwapiDataOuputter:
     def __init__(self, data):
         self.data = data
-        self.filename = self.create_filename()
-        self.filepath = self.create_filepath()
-    
-    def create_filename(self):
+        self.filename = self._get_filename()
+        self.filepath = self._get_filepath()
+        self._create_root_dir()
+
+    def _create_root_dir(self):
+        if not os.path.exists(os.getenv("DATA_OUTPUT_PATH")):
+            os.mkdir(os.getenv("DATA_OUTPUT_PATH"))
+
+    def _get_filename(self):
         return f"swapi_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
 
-    def create_filepath(self):
-        return f"{os.getenv('DATA_OUTPUT_PATH')}/{self.create_filename()}"
+    def _get_filepath(self):
+        return f"{os.getenv('DATA_OUTPUT_PATH')}/{self._get_filename()}"
 
     def to_csv(self):
         etl.tocsv(self.data, self.filepath)
-    
+
     def to_db(self):
-        swapi_data = SwapiModel(filename=self.filename)
+        swapi_data = Person(filename=self.filename)
         swapi_data.save()
--- data_transformer/swapi/swapi_data_transformer.py
+++ data_transformer/swapi/swapi_data_transformer.py
@@ -1,17 +1,30 @@
 # swapi_data_transformer.py
+from datetime import datetime
+
 import petl as etl
 
 
 class SwapiDataTransformer:
     def __init__(self, data):
         self.data = data
-        
-    
-    
-    
+
     def transform_data(self):
         table = etl.fromdicts(self.data)
-        table = etl.addfield(table, 'date', lambda row: row['edited'][:10])
-        table = etl.cutout(table, 'edited', 'url', 'films', 'species', 'vehicles', 'starships')
-        table = etl.join(table, 'homeworld', 'https://swapi.co/api/planets/{id}/', key='id', lookup='name')
+        table = table.convert(
+            "edited",
+            lambda d: datetime.strptime(d, "%Y-%m-%dT%H:%M:%S.%fZ").strftime(
+                "%Y-%m-%d"
+            ),
+        )
+        table = table.rename({"homeworld": "homeworld_url"})
+        table = table.joinlookup(
+            etl.fromjson("https://swapi.dev/api/planets/", "results"),
+            "homeworld_url",
+            "url",
+            "name",
+        )
+        table = table.cutout(
+            "films", "species", "vehicles", "starships", "created", "edited", "url"
+        )
+
         return table

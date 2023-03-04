44e368951656d27e6f0fe55e922f3da4549ba0c0
sbontop <43821319+sbontop@users.noreply.github.com>
2023-03-04 03:19:41 +0100

    Revert "applied POC to all fields from csv file"
    This reverts commit 26338bd7d74393b99f2b7d29f54c4f2261dab627.

--- swapi/urls.py
+++ swapi/urls.py
@@ -1,19 +1,9 @@
 from django.urls import path
 
-from .views import (
-    compare_fields,
-    download_data_view,
-    get_column_options,
-    preview_dataset,
-)
+from .views import compare_fields, download_data_view, preview_dataset
 
 urlpatterns = [
     path("download_data/", download_data_view, name="download_data"),
     path("preview_dataset/<str:filename>", preview_dataset, name="preview_dataset"),
     path("compare_fields/<str:filename>", compare_fields, name="compare_fields"),
-    path(
-        "get_column_options/<str:filename>",
-        get_column_options,
-        name="get_column_options",
-    ),
 ]

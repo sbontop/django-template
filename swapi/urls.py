c1d42702974ccc10b839a5d193868bf0a4bb3217
sbontop <43821319+sbontop@users.noreply.github.com>
2023-03-04 03:19:41 +0100

    Revert "Value Count POC"
    This reverts commit c6054afca4487017d80cf213a9050425b1ed5f23.

--- swapi/urls.py
+++ swapi/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import compare_fields, download_data_view, preview_dataset
+from .views import download_data_view, view_csv
 
 urlpatterns = [
     path("download_data/", download_data_view, name="download_data"),
-    path("preview_dataset/<str:filename>", preview_dataset, name="preview_dataset"),
-    path("compare_fields/<str:filename>", compare_fields, name="compare_fields"),
+    path("view_csv/<str:filename>", view_csv, name="view_csv"),
 ]

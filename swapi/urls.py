c6054afca4487017d80cf213a9050425b1ed5f23
Samuel Braganza Z <sbraganz@fiec.espol.edu.ec>
2023-03-04 01:54:52 +0100

    Value Count POC

--- swapi/urls.py
+++ swapi/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import download_data_view, view_csv
+from .views import compare_fields, download_data_view, preview_dataset
 
 urlpatterns = [
     path("download_data/", download_data_view, name="download_data"),
-    path("view_csv/<str:filename>", view_csv, name="view_csv"),
+    path("preview_dataset/<str:filename>", preview_dataset, name="preview_dataset"),
+    path("compare_fields/<str:filename>", compare_fields, name="compare_fields"),
 ]

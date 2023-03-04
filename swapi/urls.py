33cf93b7dd85e8eba5e8c530495d39421d281172
sbontop <43821319+sbontop@users.noreply.github.com>
2023-03-04 03:19:41 +0100

    Revert "WIP"
    This reverts commit 1f1e26fd38125fc91ab3dedc31371029f3b9b096.

--- swapi/urls.py
+++ swapi/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("etl/", views.SwapiETL.as_view(), name="swapi_etl"),
-    # path("list/", views.SwapiListView.as_view(), name="swapi_list"),
-    path("download_data/", views.download_swapi_data, name="swapi_download"),
+    path("etl/", views.SwapiETL.as_view(), name="swapi_etl"),
+    path("list/", views.SwapiListView.as_view(), name="swapi_list"),
 ]

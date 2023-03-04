d2e2cc8d6be4b5fba3fafb0e01417b10aac17ad4
Samuel Braganza Z <sbraganz@fiec.espol.edu.ec>
2023-03-04 01:54:52 +0100

    Value Count POC

--- swapi/views.py
+++ swapi/views.py
@@ -7,7 +7,7 @@ import requests
 from django.contrib import messages
 from django.core.files.storage import default_storage
 from django.core.paginator import Paginator
-from django.http import HttpResponseServerError
+from django.http import HttpResponseServerError, JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils import timezone
@@ -89,13 +89,11 @@ def download_data_view(request):
         return redirect(reverse("download_data"))
 
     datasets = Dataset.objects.all()
-    print(datasets)
     return render(request, "index.html", {"datasets": datasets})
 
 
-def view_csv(request, *args, **kwargs):
+def preview_dataset(request, *args, **kwargs):
     # Load CSV data
-
     filename = kwargs.get("filename")
     file_path = f"swapi/{filename}"
     with default_storage.open(file_path, "r") as f:
@@ -108,3 +106,42 @@ def view_csv(request, *args, **kwargs):
     page_obj = paginator.get_page(page_number)
 
     return render(request, "csv.html", {"page_obj": page_obj, "csv_file": file_path})
+
+
+def compare_fields(request, *args, **kwargs):
+    # Load CSV data
+    filename = kwargs.get("filename")
+    file_path = f"swapi/{filename}"
+    with default_storage.open(file_path, "r") as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+
+    if request.method != "POST":
+        return render(request, "example.html", {"rows": rows, "filename": filename})
+
+    selected_columns = request.POST.get("selected_columns")
+    selected_columns = selected_columns.split(",") if selected_columns else []
+
+    # Filter rows to selected columns
+    filtered_rows = []
+    for row in rows:
+        filtered_row = {column: row[column] for column in selected_columns}
+        filtered_rows.append(filtered_row)
+
+    # Count occurrences of values for selected columns
+    value_counts = {}
+    for row in filtered_rows:
+        key = tuple(row.values())
+        value_counts[key] = value_counts.get(key, 0) + 1
+
+    # Create table rows with value counts
+    table_rows = []
+    for key, count in value_counts.items():
+        row_dict = {column: key[i] for i, column in enumerate(selected_columns)}
+        row_dict["count"] = count
+        table_rows.append(row_dict)
+
+    # Sort table rows by count in descending order
+    table_rows = sorted(table_rows, key=lambda row: row["count"], reverse=True)
+
+    return JsonResponse({"rows": table_rows, "filename": filename})
